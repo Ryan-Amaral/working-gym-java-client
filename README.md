f86a600cca29ff360ab5f2f1a553832f28b85a90
Ryan Amaral <ryan_amaral@live.com>
2018-05-20 22:56:14 -0300

    added in code docs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # working-gym-java-client
 A java client to access the OpenAI Gym HTTP Server. (Not yet working)
+
+Put instructions for setup here, including org json.
--- src/javaclient/GymJavaHttpClient.java
+++ src/javaclient/GymJavaHttpClient.java
@@ -6,32 +6,57 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 import java.util.Set;
-import org.json.JSONObject;
 
-//much based on: https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+import org.json.*;
+
+/**
+ * Contains methods that correspond with the OpenAI Gym HTTP API 
+ * (https://github.com/openai/gym-http-api), check there for more details about the methods.
+ * @author Ryan Amaral - (ryan-amaral on GitHub)
+ */
 public class GymJavaHttpClient {
 
-    private String baseUrl;
-    private HttpURLConnection con;
+    private String baseUrl; // probably "http://127.0.0.1:5000"
+    private HttpURLConnection con; // object to use to create and do stuff with connection
 
-    public GymJavaHttpClient(String url) {
-        baseUrl = url;
+    /**
+     * Creates a client with the default base url ("http://127.0.0.1:5000").
+     */
+    public GymJavaHttpClient() {
+    	this.baseUrl = "http://127.0.0.1:5000";
+    }
+    
+    /**
+     * Creates a client with selected baseUrl.
+     * @param baseUrl Ex: "http://127.0.0.1:5000"
+     */
+    public GymJavaHttpClient(String baseUrl) {
+        this.baseUrl = baseUrl;
     }
 
+    /**
+     * List all of the environments you started that are currently running on the server.
+     * @return A set of the environments' instance Id's.
+     */
     public Set<String> listEnvs() {
         connect("/v1/envs/", "GET", null);
         return getJson().getJSONObject("all_envs").keySet();
     }
 
+    /**
+     * Creates a new environment of the type specified.
+     * @param envId The id of the environment to create (ex: "CartPole-v0").
+     * @return The instance id of the created environment.
+     */
     public String createEnv(String envId) {
         connect("/v1/envs/", "POST", "{\"env_id\":\"" + envId + "\"}");
         return getJson().getString("instance_id");
     }
 
     /**
-     * Update to return Observation.
-     * 
-     * @param instanceId
+     * Resets the selected environment.
+     * @param instanceId The id of the environment.
+     * @return observation
      */
     public void resetEnv(String instanceId) {
         connect("/v1/envs/" + instanceId + "/reset/", "POST", "{\"instance_id\":\"" + instanceId + "\"}");
@@ -39,9 +64,11 @@ public class GymJavaHttpClient {
     }
 
     /**
-     * Return info and stuff later.
-     * 
-     * @param instanceId
+     * Steps the environment.
+     * @param instanceId The id of the environment.
+     * @param action The action to do in the step.
+     * @param isDiscreteSpace Whether space in the environment is discrete or not.
+     * @return stuff
      */
     public void stepEnv(String instanceId, double action, boolean isDiscreteSpace) {
         if (isDiscreteSpace) {
@@ -56,9 +83,9 @@ public class GymJavaHttpClient {
     }
 
     /**
-     * return info later.
-     * 
-     * @param instanceId
+     * Gets the name and the dimensions of the environment's action space.
+     * @param instanceId The id of the environment.
+     * @return info
      */
     public void actionSpace(String instanceId) {
         connect("/v1/envs/" + instanceId + "/action_space/", "GET", "{\"instance_id\":\"" + instanceId + "\"}");
@@ -66,35 +93,60 @@ public class GymJavaHttpClient {
     }
 
     /**
-     * return info later.
-     * 
-     * @param instanceId
+     * Gets the name and the dimensions of the environment's observation space.
+     * @param instanceId The id of the environment.
+     * @return info
      */
     public void observationSpace(String instanceId) {
         connect("/v1/envs/" + instanceId + "/observation_space/", "GET", "{\"instance_id\":\"" + instanceId + "\"}");
         // return info
     }
 
+    /**
+     * Start monitoring.
+     * @param instanceId The id of the environment.
+     * @param force Whether to clear existing training data.
+     * @param resume Keep data that's already in.
+     */
     public void startMonitor(String instanceId, boolean force, boolean resume) {
         connect("/v1/envs/" + instanceId + "/monitor/start/", "POST", "{\"instance_id\":\"" + instanceId
                 + "\", \"force\":" + Boolean.toString(force) + ", \"resume\":" + Boolean.toString(resume) + "}");
     }
 
+    /**
+     * Flush all monitor data to disk.
+     * @param instanceId The id of the environment.
+     */
     public void closeMonitor(String instanceId) {
         connect("/v1/envs/" + instanceId + "/monitor/close/", "POST", "{\"instance_id\":\"" + instanceId + "\"}");
     }
 
-    public void flushMonitorToDisk(String trainingDir, String apiKey, String algId) {
+    /**
+     * Probably uploads your thing to OpenAI? The method just said "Flush all monitor data to disk"
+     * on the Gym HTTP API GitHub page, but it seems to do something different, I'm new to gym so I
+     * don't really know.
+     * @param trainingDir
+     * @param apiKey
+     * @param algId
+     */
+    public void upload(String trainingDir, String apiKey, String algId) {
         connect("/v1/upload/", "POST", "{\"training_dir\":\"" + trainingDir + "\"," + "\"api_key\":\"" + apiKey + "\","
                 + "\"algorithm_id\":\"" + algId + "\"}");
     }
 
+    /**
+     * Attempts to shutdown the server.
+     */
     public void shutdownServer() {
         connect("/v1/shutdown/", "POST", null);
     }
 
-    // from:
-    // https://stackoverflow.com/questions/11901831/how-to-get-json-object-from-http-request-in-java
+    /**
+     * Get JSON from the connection: technique from: 
+     * https://stackoverflow.com/questions/11901831/how-to-get-json-object-from-http-request-in-java
+     * @return The JSON obtained from the connection, first line only, which should hopefully
+     * contain all JSON.
+     */
     private JSONObject getJson() {
         JSONObject json = null;
         try {
@@ -111,6 +163,12 @@ public class GymJavaHttpClient {
     }
 
     // https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+    /**
+     * Does either a post or get request on the base url + urlEx.
+     * @param urlEx The extension to add onto base url.
+     * @param mthd POST or GET.
+     * @param args What to pass for a Post request, make null if not used.
+     */
     private void connect(String urlEx, String mthd, String args) {
         try {
             URL url = new URL(baseUrl + urlEx);
@@ -139,9 +197,13 @@ public class GymJavaHttpClient {
         }
     }
 
+    /**
+     * Test simple stuff out here.
+     * @param args
+     */
     public static void main(String args[]) {
         GymJavaHttpClient clnt = new GymJavaHttpClient("http://127.0.0.1:5000");
-        System.out.println(clnt.createEnv("CartPole-v0"));
+        System.out.println(clnt.createEnv("gvgai-aliens-lvl0-v0"));
         System.out.println(clnt.listEnvs().toString());
     }
 
