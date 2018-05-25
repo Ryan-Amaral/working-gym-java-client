943cd2e1f80293c1f9cc4a5a14d25cb781042057
Ryan Amaral <ryan_amaral@live.com>
2018-05-25 11:50:43 -0300

    removed rediculous tab indentations

--- src/javaclient/StepObject.java
+++ src/javaclient/StepObject.java
@@ -6,18 +6,16 @@ package javaclient;
  * @author Ryan Amaral - (ryan-amaral on GitHub)
  */
 public class StepObject {
-	
-	public StepObject(Object observation, float reward, boolean done,Object info) {
-		this.observation = observation;
-		this.reward = reward;
-		this.done = done;
-		this.info = info;
-	}
-	
-	public Object observation;
-	public float reward;
-	public boolean done;
-	public Object info;
 
-	// a comment
+    public StepObject(Object observation, float reward, boolean done,Object info) {
+        this.observation = observation;
+        this.reward = reward;
+        this.done = done;
+        this.info = info;
+    }
+
+    public Object observation;
+    public float reward;
+    public boolean done;
+    public Object info;
 }
